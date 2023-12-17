a54bd264a5c89dfc95794ecf8bf38291b9c95025
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-17 16:18:00 +0530

    user service form data array added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": 'John Doe',
+            "email": ' John@april.biz',
+            "role": 'INTERN'
+        },
+        {
+            "id": 2,
+            "name": 'Ervin Doe',
+            "email": 'Shanna@melissa.tv',
+            "role": 'INTERN'
+        },
+        {
+            "id": 3,
+            "name": 'Clementine Doe',
+            "email": 'Nathen@yes.net',
+            "role": 'ENGINEER'
+        },
+        {
+            "id": 4,
+            "name": 'Patricia Doe',
+            "email": 'Juli.Oco@ko.org',
+            "role": 'ENGINEER'
+        },
+        {
+            "id": 5,
+            "name": 'Chelsey Doe',
+            "email": 'Lu@coan.ca',
+            "role": 'ADMIN'
+        }
+    ]
+}
